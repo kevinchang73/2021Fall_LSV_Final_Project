78311a16a3d2bf18601000c6fde79bf41e2147a4
Whyu1125 <kobe7682477@gmail.com>
2022-01-02 00:23:41 +0800

    add agent.py

---
+++ agent.py
@@ -0,0 +1,19 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+class Network(nn.Module):
+
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.fc2 = nn.Linear(128, 256)
+        self.fc3 = nn.Linear(256, output_dim)
+
+    def forward(self, state):
+        hid = torch.tanh(self.fc1(state))
+        hid = torch.tanh(self.fc2(hid))
+        return F.softmax(self.fc3(hid), dim = 1)
