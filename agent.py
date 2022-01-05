cdee6574782b2bb7f9f6aa150b68620124b2e912
Whyu1125 <kobe7682477@gmail.com>
2022-01-05 19:05:29 +0800

    finish main.py

--- agent.py
+++ agent.py
@@ -14,10 +14,10 @@ class Network(nn.Module):
         self.fc3 = nn.Linear(256, output_dim)
 
     def forward(self, state):
-        hid = torch.tanh(self.fc1(state))
-        hid = torch.tanh(self.fc2(hid))
+        hid = torch.relu(self.fc1(state))
+        hid = torch.relu(self.fc2(hid))
 
-        return torch.tanh(self.fc3(hid))
+        return torch.relu(self.fc3(hid))
 
 
 class Agent():
