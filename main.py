a4a7bf2bb399da05b6e073ed9dd3a19fc6e6f8de
Whyu1125 <kobe7682477@gmail.com>
2022-01-05 18:52:09 +0800

    finish main.py

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ lines = fi.readlines()[1:]
 lines = [list(map(int, l.strip().split(" "))) for l in lines]
 
 input_dim = len(lines[0])
-
-newAgent = Agent(input_dim, len(env.TLN.nodes) + len(env.TLN.edges) - len(env.TLN.pis))
+output_dim = len(env.TLN.nodes) + len(env.TLN.edges) - len(env.TLN.pis)
+print("output_dim: ", output_dim)
+newAgent = Agent(input_dim, output_dim)
 
 newAgent.network.train()
 
