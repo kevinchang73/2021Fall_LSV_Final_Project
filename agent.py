32c1cc5ab7a5a2c00c19d034502fe06f6db5ff91
Whyu1125 <kobe7682477@gmail.com>
2022-01-07 02:38:20 +0800

    adjust parameters

--- agent.py
+++ agent.py
@@ -9,9 +9,9 @@ class Network(nn.Module):
 
     def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.fc1 = nn.Linear(input_dim, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, output_dim)
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.fc2 = nn.Linear(256, 1024)
+        self.fc3 = nn.Linear(1024, output_dim)
 
     def forward(self, state):
         hid = torch.tanh(self.fc1(state))
