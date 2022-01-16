ceb90b6ec2e120694784a32f2722c0892a833eb1
Whyu1125 <kobe7682477@gmail.com>
2022-01-16 21:35:06 +0800

    update parameters

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import random
 
 input_file = sys.argv[2]
 env = Tln_env(input_file + ".tln")
-fi = open(input_file + ".funct", "r")
+fi = open(input_file + ".funct2", "r")
 lines = fi.readlines()[1:]
 lines = [list(map(int, l.strip().split(" "))) for l in lines]
 print("Number of functions in training set: ", len(lines))
