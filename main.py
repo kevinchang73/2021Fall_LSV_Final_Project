d570912e0497e3f87d4a0ef030c279644755a919
Whyu1125 <kobe7682477@gmail.com>
2022-01-16 18:05:14 +0800

    update parameters

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ for batch in prg_bar:
     # avg_final_reward = sum(final_rewards) / len(final_rewards)
     # avg_total_rewards.append(avg_total_reward)
     # avg_final_rewards.append(avg_final_reward)
-    prg_bar.set_description(f"loss: {loss: 0.6f}")
+    prg_bar.set_description(f"loss: {loss.numpy().tolist()[0]: 0.6f}")
     # print("CrossEntropy: {CrossEntropy: 0.6f}")
 
     # 更新網路
