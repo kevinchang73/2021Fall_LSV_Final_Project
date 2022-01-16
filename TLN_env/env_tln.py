2596ef62d2bb0e62e9c60d9127b70eb86e4b8e7a
Whyu1125 <kobe7682477@gmail.com>
2022-01-16 22:40:52 +0800

    update parameters

--- TLN_env/env_tln.py
+++ TLN_env/env_tln.py
@@ -68,10 +68,10 @@ class Tln_env():
         #MSELoss
         outputs = torch.tensor(SE, dtype = torch.float)
         outputs.requires_grad = True
-        print(outputs)
+        print("outputs: ", outputs)
         target = torch.tensor(output_values, dtype = torch.float)
         target.requires_grad = True
-        print(target)
+        print("target: ", target)
         # MSE = torch.from_numpy(MSELoss)
         # MSE.requires_grad = True
         return nn.MSELoss()(outputs, target)
