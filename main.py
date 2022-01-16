c111d1c6cb514abdb111fbfd7526aebe8aa8c6ea
Whyu1125 <kobe7682477@gmail.com>
2022-01-16 23:50:14 +0800

    update parameters

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ for batch in prg_bar:
 
     # output_values = random.choice(lines)
     output_values = lines[0]
-    output_values = torch.tensor(output_values)
+    output_values = torch.tensor(output_values, dtype = torch.float)
     action = newAgent.sample(output_values)
     # print(action)
     # print("action: ", action)
