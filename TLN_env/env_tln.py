5b88a4dc20ecca58856f1385c0ace50a1e3c53d1
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 01:03:29 +0800

    update parameters

--- TLN_env/env_tln.py
+++ TLN_env/env_tln.py
@@ -47,7 +47,7 @@ class Tln_env():
     def step(self, action, output_values):
         # assert self.action_space.contains(action)
         # action = list(action)
-        # self.TLN.set_weights(action[0:len(self.TLN.edges)])
+        self.TLN.set_weights(action[0:len(self.TLN.edges)])
         # self.TLN.print_weights()
         # self.TLN.set_thresholds([0]*len(self.TLN.nodes))
         # outputs = torch.empty(0, dtype = torch.float)
