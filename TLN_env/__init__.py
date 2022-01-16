45eda7c500c083a26e5fdc3bb2e08fc7bb7a55cb
Whyu1125 <kobe7682477@gmail.com>
2022-01-16 14:19:45 +0800

    delete gym

--- TLN_env/__init__.py
+++ TLN_env/__init__.py
@@ -1,5 +1,5 @@
-from gym.envs.registration import register
-register(
-    id='TLNENV-v0',
-    entry_point='TLN.env.env_tln:tln',
-)
+# from gym.envs.registration import register
+# register(
+#     id='TLNENV-v0',
+#     entry_point='TLN.env.env_tln:tln',
+# )
--- TLN_env/env_tln.py
+++ TLN_env/env_tln.py
@@ -1,41 +1,41 @@
-import gym
-from gym import spaces
-from gym.utils import seeding
+# import gym
+# from gym import spaces
+# from gym.utils import seeding
 import numpy as np
 from TLN_env.TLN.TLN import Tln
 import sys
 import math
 
 
-class Tln_env(gym.Env):
+class Tln_env():
 
     def __init__(self, inputFile):
         self.range = 1000  # +/- value the randomly select guess_number can be between
-        self.weight_bound = 3  # Action space bounds
-        self.threshold_bound = 5  # Action space bounds
+        self.weight_bound = 1  # Action space bounds
+        self.threshold_bound = 1  # Action space bounds
         self.count = 0
         self.max_count = 200
         self.TLN = Tln(inputFile)
-        low, high = self.init_weight_and_threshold();
-        self.action_space = spaces.Box(low=np.array(low), high=np.array(high))
-        self.observation_space = spaces.Box(low = np.array(-1.0), high=np.array(1.0))
+        # low, high = self.init_weight_and_threshold();
+        # self.action_space = spaces.Box(low=np.array(low), high=np.array(high))
+        # self.observation_space = spaces.Box(low = np.array(-1.0), high=np.array(1.0))
         # self.prev_reward = 0
 
         # self.seed()
 
-    def init_weight_and_threshold(self):
-        """
-        TLN-weight: [w1, w2]
-        TLN-weight: [t1, t2, t3]
-        weight_bound = 3
-        threshold_bound = 5
-        low = [-3, -3, -5]
-        high = [3, 3, 5]
-        return lower bound and upper bound of action_space (1-dim array)
-        """
-        low = [-self.weight_bound]*len(self.TLN.edges) + [-self.threshold_bound]*(len(self.TLN.nodes) - 2)
-        high = [self.weight_bound]*len(self.TLN.edges) + [self.threshold_bound]*(len(self.TLN.nodes) - 2)
-        return low, high
+    # def init_weight_and_threshold(self):
+    #     """
+    #     TLN-weight: [w1, w2]
+    #     TLN-weight: [t1, t2, t3]
+    #     weight_bound = 3
+    #     threshold_bound = 5
+    #     low = [-3, -3, -5]
+    #     high = [3, 3, 5]
+    #     return lower bound and upper bound of action_space (1-dim array)
+    #     """
+    #     low = [-self.weight_bound]*len(self.TLN.edges) + [-self.threshold_bound]*(len(self.TLN.nodes) - 2)
+    #     high = [self.weight_bound]*len(self.TLN.edges) + [self.threshold_bound]*(len(self.TLN.nodes) - 2)
+    #     return low, high
 
     # def seed(self, seed=None):
     #     self.np_random, seed = seeding.np_random(seed)
