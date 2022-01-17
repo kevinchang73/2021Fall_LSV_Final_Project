ec2d78535e6621a4bf3c0713e25b6f1fe4c8d539
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 22:08:14 +0800

    update parameters

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ y = []
 i = 0;
 for batch in prg_bar:
     newAgent.optimizer.zero_grad()
-    # output_values = random.choice(lines)
-    output_values = lines[0]
+    output_values = random.choice(lines)
+    # output_values = lines[0]
     output_values = torch.tensor(output_values, dtype = torch.float)
     output_values.requires_grad = True
     weight = newAgent.sample(output_values)
