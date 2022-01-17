40f5c3931b683d378a263ee2ca13545cbc4ac2e9
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 18:29:17 +0800

    update parameters

--- main.py
+++ main.py
@@ -56,10 +56,10 @@ for batch in prg_bar:
     output_values = torch.tensor(output_values, dtype = torch.float)
     output_values.requires_grad = True
     weight = newAgent.sample(output_values)
-    # weight_sum = weight.sum()
-    # weight_sum.retain_grad()
-    # weight_sum.backward()
-    
+    weight_sum = weight.sum()
+    weight_sum.retain_grad()
+    weight_sum.backward()
+    print("##################")
     for name, params in newAgent.network.named_parameters():
         # params.retain_grad()
         # print("name: ", name)
@@ -68,7 +68,7 @@ for batch in prg_bar:
         print("grad: ", params.grad)
     # print(weight_sum.grad)
     # print(output_values.grad)
-    loss = env.step(weight, output_values)
+    # loss = env.step(weight, output_values)
     # print(weight)
     # print(loss)
     # i += 1
@@ -78,7 +78,7 @@ for batch in prg_bar:
     #     params.retain_grad()
     #     # print("name: ", name)
     #     # print("para: ", params)
-    newAgent.learn(Variable(loss, requires_grad = True))
+    # newAgent.learn(Variable(loss, requires_grad = True))
     # for name, params in newAgent.network.named_parameters():
     #     # print("name: ", name)
     #     # print("para: ", params)
