b3a846643463ddd4773af8eac750a431d9177fe6
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 23:09:52 +0800

    update parameters

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ for j in range(NUM_EPOCH):
         # output_values = lines[i]
         # output_values = torch.tensor(output_values, dtype = torch.float)
         output_values = data[0]
+        print(output_values)
         for k in range(1, BATCH_SIZE):
             output_values.extend(data[k])
         output_values = torch.tensor(output_values, dtype = torch.float)
