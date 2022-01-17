e544a3592e7193c192a996890bbaef0073742931
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 20:36:56 +0800

    update parameters

--- main.py
+++ main.py
@@ -56,10 +56,10 @@ for batch in prg_bar:
     output_values = torch.tensor(output_values, dtype = torch.float)
     output_values.requires_grad = True
     weight = newAgent.sample(output_values)
-    weight_sum = weight.sum()
-    weight_sum.retain_grad()
-    weight_sum.backward()
-    newAgent.optimizer.step()
+    # weight_sum = weight.sum()
+    # weight_sum.retain_grad()
+    # weight_sum.backward()
+    # newAgent.optimizer.step()
     print("##################")
     for name, params in newAgent.network.named_parameters():
         # params.retain_grad()
@@ -69,7 +69,7 @@ for batch in prg_bar:
         print("grad: ", params.grad)
     # print(weight_sum.grad)
     # print(output_values.grad)
-    # loss = env.step(weight, output_values)
+    loss = env.step(weight, output_values)
     # print(weight)
     # print(loss)
     # i += 1
@@ -79,13 +79,13 @@ for batch in prg_bar:
     #     params.retain_grad()
     #     # print("name: ", name)
     #     # print("para: ", params)
-    # newAgent.learn(Variable(loss, requires_grad = True))
+    newAgent.learn(loss)
 
-    # for name, params in newAgent.network.named_parameters():
-    #     # print("name: ", name)
-    #     # print("para: ", params)
-    #     print("required_grad: ", params.requires_grad)
-    #     print("grad: ", params.grad)
+    for name, params in newAgent.network.named_parameters():
+        # print("name: ", name)
+        print("para: ", params)
+        print("required_grad: ", params.requires_grad)
+        print("grad: ", params.grad)
     # print(weight.requires_grad)
     # print(weight.grad)
     # print(output_values.requires_grad)
