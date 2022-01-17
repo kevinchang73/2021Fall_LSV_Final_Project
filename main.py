3022f4ea13cf09a95ed46c28e2c8fa3ed54fda35
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 23:59:46 +0800

    update parameters

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 NUM_EPOCH = 5
-BATCH_SIZE = 1
+BATCH_SIZE = 5
 class TLNDateset(Dataset):
     def __init__(self, X):
         self.data = torch.tensor(X, dtype = torch.float)
@@ -38,43 +38,47 @@ newAgent.network.train()
 x = []
 total_loss = []
 prg_bar = tqdm(enumerate(train_loader))
-for i in range(1000):
-    newAgent.optimizer.zero_grad()
-    output_values = torch.tensor(lines[0], dtype = torch.float)
-    output_values.requires_grad = True
-    weight = newAgent.sample(output_values)
-    loss = env.step(weight, output_values)
-    print(loss)
-    newAgent.learn(loss)
-# for epoch in range(NUM_EPOCH):
-#     train_loss = 0
-#     for i, data in prg_bar:
-#         newAgent.optimizer.zero_grad()
-#         # output_values = random.choice(lines)
-#         # output_values = lines[i]
-#         # output_values = torch.tensor(output_values, dtype = torch.float)
-#         output_values = data[0]
-#         for k in range(1, BATCH_SIZE):
-#             output_values = torch.cat((output_values, data[k]), 0)
-#         output_values.requires_grad = True
+
+#CONFIRM MODEL RUNNING 
+# for i in range(1000):
+#     newAgent.optimizer.zero_grad()
+#     output_values = torch.tensor(lines[0], dtype = torch.float)
+#     output_values.requires_grad = True
+#     weight = newAgent.sample(output_values)
+#     loss = env.step(weight, output_values)
+#     print(loss)
+#     newAgent.learn(loss)
+
+#Start Training
+for epoch in range(NUM_EPOCH):
+    train_loss = 0
+    for i, data in prg_bar:
+        newAgent.optimizer.zero_grad()
+        # output_values = random.choice(lines)
+        # output_values = lines[i]
+        # output_values = torch.tensor(output_values, dtype = torch.float)
+        output_values = data[0]
+        for k in range(1, BATCH_SIZE):
+            output_values = torch.cat((output_values, data[k]), 0)
+        output_values.requires_grad = True
         
-#         weight = newAgent.sample(output_values)
-#         # print(weight)
-#         loss = env.step(weight, output_values)
-#         # print(loss.item())
-#         newAgent.learn(loss)
-#         train_loss += loss.item()
-#         # print(train_loss)
+        weight = newAgent.sample(output_values)
+        # print(weight)
+        loss = env.step(weight, output_values)
+        # print(loss.item())
+        newAgent.learn(loss)
+        train_loss += loss.item()
+        # print(train_loss)
 
-#         # print("###############")
-#         # for name, params in newAgent.network.named_parameters():
-#         #     print("params: ", params)
-#         #     print("params grad: ", params.grad)
+        print("###############")
+        for name, params in newAgent.network.named_parameters():
+            print("params: ", params)
+        #     print("params grad: ", params.grad)
 
 
-#         prg_bar.set_description(f"loss:  {loss.item(): .6f}")
-#     x.append(epoch + 1)
-#     total_loss.append(train_loss/len(train_set)*BATCH_SIZE)
+        prg_bar.set_description(f"loss:  {loss.item(): .6f}")
+    x.append(epoch + 1)
+    total_loss.append(train_loss/len(train_set)*BATCH_SIZE)
     
 plt.plot(x, total_loss)
 plt.savefig(input_file + ".jpg")
