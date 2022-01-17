521a22d6791d9ddeed6e1410880d2de0d2e5106b
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 23:57:19 +0800

    update parameters

--- main.py
+++ main.py
@@ -38,35 +38,43 @@ newAgent.network.train()
 x = []
 total_loss = []
 prg_bar = tqdm(enumerate(train_loader))
-for epoch in range(NUM_EPOCH):
-    train_loss = 0
-    for i, data in prg_bar:
-        newAgent.optimizer.zero_grad()
-        # output_values = random.choice(lines)
-        # output_values = lines[i]
-        # output_values = torch.tensor(output_values, dtype = torch.float)
-        output_values = data[0]
-        for k in range(1, BATCH_SIZE):
-            output_values = torch.cat((output_values, data[k]), 0)
-        output_values.requires_grad = True
+for i in range(1000):
+    newAgent.optimizer.zero_grad()
+    output_values = torch.tensor(lines[0], dtype = torch.float)
+    output_values.requires_grad = True
+    weight = newAgent.sample(output_values)
+    loss = env.step(weight, output_values)
+    print(loss)
+    newAgent.learn(loss)
+# for epoch in range(NUM_EPOCH):
+#     train_loss = 0
+#     for i, data in prg_bar:
+#         newAgent.optimizer.zero_grad()
+#         # output_values = random.choice(lines)
+#         # output_values = lines[i]
+#         # output_values = torch.tensor(output_values, dtype = torch.float)
+#         output_values = data[0]
+#         for k in range(1, BATCH_SIZE):
+#             output_values = torch.cat((output_values, data[k]), 0)
+#         output_values.requires_grad = True
         
-        weight = newAgent.sample(output_values)
-        # print(weight)
-        loss = env.step(weight, output_values)
-        # print(loss.item())
-        newAgent.learn(loss)
-        train_loss += loss.item()
-        # print(train_loss)
+#         weight = newAgent.sample(output_values)
+#         # print(weight)
+#         loss = env.step(weight, output_values)
+#         # print(loss.item())
+#         newAgent.learn(loss)
+#         train_loss += loss.item()
+#         # print(train_loss)
 
-        # print("###############")
-        # for name, params in newAgent.network.named_parameters():
-        #     print("params: ", params)
-        #     print("params grad: ", params.grad)
+#         # print("###############")
+#         # for name, params in newAgent.network.named_parameters():
+#         #     print("params: ", params)
+#         #     print("params grad: ", params.grad)
 
 
-        prg_bar.set_description(f"loss:  {loss.item(): .6f}")
-    x.append(epoch + 1)
-    total_loss.append(train_loss/len(train_set)*BATCH_SIZE)
+#         prg_bar.set_description(f"loss:  {loss.item(): .6f}")
+#     x.append(epoch + 1)
+#     total_loss.append(train_loss/len(train_set)*BATCH_SIZE)
     
 plt.plot(x, total_loss)
 plt.savefig(input_file + ".jpg")
