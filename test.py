f5b1891834b9cea5c50997aab9a2e861d3bb5986
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 16:12:46 +0800

    update parameters

--- test.py
+++ test.py
@@ -1,14 +1,47 @@
+import torch
 
-import sys
+print("Trial 1: with python float")
+w = torch.randn(3,5,requires_grad = True) * 0.01
 
-input_file = sys.argv[2]
+x = torch.randn(5,4,requires_grad = True)
 
-# env = Tln_env(input_file + ".tln")
+y = torch.matmul(w,x).sum(1)
 
-fi = open(input_file + ".funct", "r")
+y.backward(torch.ones(3))
 
-lines = fi.readlines()[1:]
+print("w.requires_grad:",w.requires_grad)
+print("x.requires_grad:",x.requires_grad)
 
-lines = [list(map(int, l.strip().split(" "))) for l in lines]
+print("w.grad",w.grad)
+print("x.grad",x.grad)
 
-print(lines)
+print("Trial 2: with on-the-go torch scalar")
+w = torch.randn(3,5,requires_grad = True) * torch.tensor(0.01,requires_grad=True)
+
+x = torch.randn(5,4,requires_grad = True)
+
+y = torch.matmul(w,x).sum(1)
+
+y.backward(torch.ones(3))
+
+print("w.requires_grad:",w.requires_grad)
+print("x.requires_grad:",x.requires_grad)
+
+print("w.grad",w.grad)
+print("x.grad",x.grad)
+
+print("Trial 3: with named torch scalar")
+t = torch.tensor(0.01,requires_grad=True)
+w = torch.randn(3,5,requires_grad = True) * t
+
+x = torch.randn(5,4,requires_grad = True)
+
+y = torch.matmul(w,x).sum(1)
+
+y.backward(torch.ones(3))
+
+print("w.requires_grad:",w.requires_grad)
+print("x.requires_grad:",x.requires_grad)
+
+print("w.grad",w.grad)
+print("x.grad",x.grad)
