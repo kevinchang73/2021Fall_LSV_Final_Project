6184cce72bd111be55982ee81787d58e3ca9c733
kevinchang73 <ckc8346368@gmail.com>
2022-01-17 14:39:06 +0800

    Update direct NN

--- Direct_NN/network.py
+++ Direct_NN/network.py
@@ -1,15 +1,12 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.distributions import Categorical
+from manipulateTLN import *
 
 class Network(nn.Module):
-    def __init__(self): #Todo: convert TLN to NN
+    def __init__(self, manTLN): #Todo: convert TLN to NN
         super(Network, self).__init__()
 
     def forward(self, x):
 
     def cal_loss(self, pred, target):
-        
