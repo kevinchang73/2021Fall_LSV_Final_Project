3021c5cd72b93ce8a966980a3d9c3f53aff5204b
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 21:28:00 +0800

    update parameters

--- TLN_env/TLN/TLN.py
+++ TLN_env/TLN/TLN.py
@@ -26,7 +26,7 @@ class Node:
             # sum += edge.weight * edge.value
         assert(torch.is_tensor(self.threshold))
         weight_x_value_sum = weight_x_value.sum()
-        self.value = 1/(1 + torch.log(weight_x_value_sum - self.threshold))
+        self.value = 1/(1 + torch.log(self.threshold - weight_x_value_sum))
         print(self.value.requires_grad)
         # if sum >= self.threshold:
         #     self.value = torch.tensor(1.0, dtype = torch.float)
