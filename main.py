97eb2d3696d75da5ff3b93bdc403f89149508c3f
Whyu1125 <kobe7682477@gmail.com>
2022-01-17 17:21:51 +0800

    update parameters

--- main.py
+++ main.py
@@ -55,24 +55,24 @@ for batch in prg_bar:
     output_values = lines[0]
     output_values = torch.tensor(output_values, dtype = torch.float)
     output_values.requires_grad = True
-    output_values.retain_grad()
     weight = newAgent.sample(output_values)
     loss = env.step(weight, output_values)
-    # for name, params in newAgent.network.named_parameters():
-    #     # print("name: ", name)
-    #     # print("para: ", params)
-    #     print("required_grad: ", params.requires_grad)
-    #     print("grad: ", params.grad)
+    for name, params in newAgent.network.named_parameters():
+        params.retain_grad()
+        # print("name: ", name)
+        # print("para: ", params)
+        print("required_grad: ", params.requires_grad)
+        print("grad: ", params.grad)
     # print(weight)
     # print(loss)
     # i += 1
     # if(i%10 == 0):
     #     y.append(loss)
     newAgent.learn(Variable(loss, requires_grad = True))
-    print(weight.requires_grad)
-    print(weight.grad)
-    print(output_values.requires_grad)
-    print(output_values.grad)
+    # print(weight.requires_grad)
+    # print(weight.grad)
+    # print(output_values.requires_grad)
+    # print(output_values.grad)
 # output_values = lines[0]
 # output_values = torch.tensor(output_values, dtype = torch.float)
 # weight = newAgent.sample(output_values)
