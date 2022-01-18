0c59b1c8e781fd6c55020840bf8455fc131d0cec
Whyu1125 <kobe7682477@gmail.com>
2022-01-18 18:31:23 +0800

    update parameters

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ import time
 
 model_path = "./model.ckpt"
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-NUM_EPOCH = 100
-BATCH_SIZE = 100
+print(device)
+NUM_EPOCH = 5
+BATCH_SIZE = 2
 TRAINING_DATA_RATIO = 0.8
 class TLNDateset(Dataset):
     def __init__(self, X):
@@ -32,7 +33,7 @@ fi = open(input_file + ".funct2", "r")
 lines = fi.readlines()[1:]
 lines = [list(map(int, l.strip().split(" "))) for l in lines]
 print("Number of functions in training set: ", len(lines))
-# lines = lines[:10]
+lines = lines[:10]
 random.shuffle(lines)
 train_lines = lines[:int(len(lines)*TRAINING_DATA_RATIO)]
 test_lines = lines[int(len(lines)*TRAINING_DATA_RATIO):]
@@ -41,7 +42,7 @@ test_set = TLNDateset(test_lines)
 train_loader = DataLoader(train_set, batch_size = BATCH_SIZE, shuffle = True)
 test_loader = DataLoader(test_set, batch_size = BATCH_SIZE, shuffle = True)
 
-input_dim = len(lines[0])*BATCH_SIZE
+input_dim = len(lines[0])
 output_dim = len(env.TLN.edges)
 newAgent = Agent(input_dim, output_dim)
 
@@ -63,38 +64,39 @@ total_loss = []
 
 # Start Training
 for epoch in range(NUM_EPOCH):
-    train_loss = 0
+    train_loss = 0.0
     prg_bar = tqdm(enumerate(train_loader))
     for i, data in prg_bar:
+        batch_loss = torch.tensor(0.0, dtype = torch.float).to(device)
         newAgent.optimizer.zero_grad()
-        # output_values = random.choice(lines)
-        # output_values = lines[i]
-        # output_values = torch.tensor(output_values, dtype = torch.float)
-        output_values = data[0]
-        for k in range(1, BATCH_SIZE):
-            output_values = torch.cat((output_values, data[k]), 0)
-        # output_values.requires_grad = True
-        output_values_device = output_values.to(device)
-        weight = newAgent.sample(output_values_device)
-        # print(weight)
-        # print(weight)
-        loss = env.step(weight, output_values_device)
-        # print(loss.item())
-        newAgent.learn(loss)
-        train_loss += loss.item()
-        # print(train_loss)
+        for b in range(BATCH_SIZE):
+            # output_values = random.choice(lines)
+            # output_values = lines[i]
+            # output_values = torch.tensor(output_values, dtype = torch.float)
+            output_values = data[b]
+            # output_values.requires_grad = True
+            output_values_device = output_values.to(device)
+            weight = newAgent.sample(output_values_device)
+            # print(weight)
+            # print(weight)
+            loss = env.step(weight, output_values_device)
+            # print(loss.item())
+            batch_loss = torch.add(batch_loss, loss)
+            # print(train_loss)
 
-        # print("###############")
-        # for name, params in newAgent.network.named_parameters():
-        #     print("params: ", params)
-        #     print("params grad: ", params.grad)
+            # print("###############")
+            # for name, params in newAgent.network.named_parameters():
+            #     print("params: ", params)
+            #     print("params grad: ", params.grad)
 
 
-        prg_bar.set_description(f"loss:  {loss.item(): .6f}")
+        newAgent.learn(batch_loss)
+        train_loss += batch_loss.item()/BATCH_SIZE
+        prg_bar.set_description(f"loss:  {batch_loss.item()/BATCH_SIZE: .6f}")
     x.append(epoch + 1)
-    total_loss.append(train_loss/len(train_set)*BATCH_SIZE)
-    print("Average training loss: ", train_loss/len(train_set)*BATCH_SIZE)
-    f.write(str(train_loss/len(train_set)*BATCH_SIZE) + '\n')
+    total_loss.append(train_loss/len(train_set))
+    print("Average training loss: ", train_loss/len(train_set))
+    f.write(str(train_loss/len(train_set)) + '\n')
 
 print("x: ", x)
 print("total_loss: ", total_loss)
@@ -105,23 +107,42 @@ f = open(input_file + " " + t + " test", "w")
 print("Start Testing")
 newAgent.network.eval()
 env.TLN.set_tests(True)
-total_test_loss = []
+total_loss = []
 with torch.no_grad():
     for epoch in range(NUM_EPOCH):
-        prg_bar = tqdm(enumerate(test_loader))
         test_loss = 0.0
+        prg_bar = tqdm(enumerate(test_loader))
         for i, data in prg_bar:
-            output_values = data[0]
-            for k in range(1, BATCH_SIZE):
-                output_values = torch.cat((output_values, data[k]), 0)
-            output_values_device = output_values.to(device)
-            weight = newAgent.sample(output_values_device)
-            loss = env.step(weight, output_values_device)
-            test_loss += loss.item()
-            prg_bar.set_description(f"loss:  {loss.item(): .6f}")
-        total_test_loss.append(test_loss/len(test_set)*BATCH_SIZE)
-        print(test_loss/len(test_set)*BATCH_SIZE)
-        f.write(str(test_loss/len(test_set)*BATCH_SIZE) + '\n')
+            batch_loss = torch.tensor(0.0, dtype = torch.float).to(device)
+            newAgent.optimizer.zero_grad()
+            for b in range(BATCH_SIZE):
+                # output_values = random.choice(lines)
+                # output_values = lines[i]
+                # output_values = torch.tensor(output_values, dtype = torch.float)
+                output_values = data[b]
+                # output_values.requires_grad = True
+                output_values_device = output_values.to(device)
+                weight = newAgent.sample(output_values_device)
+                # print(weight)
+                # print(weight)
+                loss = env.step(weight, output_values_device)
+                # print(loss.item())
+                batch_loss = torch.add(batch_loss, loss)
+                # print(train_loss)
+
+                # print("###############")
+                # for name, params in newAgent.network.named_parameters():
+                #     print("params: ", params)
+                #     print("params grad: ", params.grad)
+
+
+            newAgent.learn(batch_loss)
+            test_loss += batch_loss.item()/BATCH_SIZE
+            prg_bar.set_description(f"loss:  {batch_loss.item()/BATCH_SIZE: .6f}")
+        x.append(epoch + 1)
+        total_loss.append(test_loss/len(test_set))
+        print("Average training loss: ", test_loss/len(test_set))
+        f.write(str(test_loss/len(test_set)) + '\n')
 print("Testing Done")
 
 
