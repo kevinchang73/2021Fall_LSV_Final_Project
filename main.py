4ad319cb83d07f9b2f11439d0144d14c8ad5d892
Whyu1125 <kobe7682477@gmail.com>
2022-01-20 01:10:40 +0800

    update parameters

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class TLNDateset(Dataset):
 
 input_file = sys.argv[2]
 env = Tln_env(input_file + ".tln")
-fi = open(input_file + ".funct2", "r")
+fi = open(input_file + ".funct", "r")
 lines = fi.readlines()[1:]
 lines = [list(map(int, l.strip().split(" "))) for l in lines]
 random.shuffle(lines)
